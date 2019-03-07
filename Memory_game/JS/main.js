2a29e347b0a93d3057c670277a7bb7aee43d11c6
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-03-07 12:34:50 -0700

    created game logic

--- Memory_game/JS/main.js
+++ Memory_game/JS/main.js
@@ -1,7 +1,22 @@
 console.log("up and running!");
-var cardFour = "King";
-var cardThree = "Queen";
-var cardTwo = "King";
-var cardOne = "Queen";
 
-console.log("User flipped " + cardOne);
+
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsinplay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsinplay.push(cardOne, cardTwo);
+
+if (cardsinplay.length === [2] ) {
+  if else {
+    cardsinplay[0] = cardsinplay[2];
+    alert("You found a match!");
+  }
+  alert("You found a match!");
+  if else {
+    cardsinplay[0] === cardsinplay[1,3]
+    alert("Sorry, Try again.");
+  }
+}
+
+console.log("User flipped " + cardsinplay);
